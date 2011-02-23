896914fb9c10739a2c6e4d625ac18ccf4772cea6
lgbpinho153 <lgbpinho153@yahoo.com.br>
2011-02-23 00:15:53 +0000

    Finished

---  neural-networks-using-r/simplest case widrow-hoff rule .R
+++  neural-networks-using-r/simplest case widrow-hoff rule .R
@@ -6,19 +6,23 @@
 
 ### Simple example data
 In<-cbind(rnorm(100,0,1),rnorm(100,0,1)) # One case per row, one input per column
-Target<-2*In[,1]+3*In[,2]
+Target<-10+2*In[,1]+3*In[,2]
 
 ### Parameters
 NumInputs<-dim(In)[2] # Number of input nodes
 NumOutputs<-dim(In)[2] # Number of output nodes
 Weights<-rep(0,NumInputs+1) # Bias and weights vector
 Lr<-0.05 # Learning rate
+Delta<-rep(0,NumInputs+1) # Weight adjustment vector
+NumEpochs<-1000
 
 ### Estimation
 NeuronIn<-cbind(rep(1,100),In)
-NeuronSum<-NeuronIn%*%Weights
-Out<-NeuronSum # The output for each case in each row
-Error<-Target-Out
-Delta<-Lr*Error*In
-
+for (j in 1:NumEpochs){
+	NeuronSum<-NeuronIn%*%Weights
+	Out<-NeuronSum # The output for each case in each row
+	Error<-Target-Out
+	for (i in 1:(NumInputs+1)) Delta[i]<-sum(Lr*Error*NeuronIn[,i])/100
+	Weights<-Weights+Delta
+}
 
