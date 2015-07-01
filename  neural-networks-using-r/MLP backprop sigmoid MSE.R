e640398936a33dbbc8685dbbf8d176994e89dcb7
Vikas Gupta <vikas.ymca@gmail.com>
2015-07-01 00:35:31 -0500

    some formatting

---  neural-networks-using-r/MLP backprop sigmoid MSE.R
+++  neural-networks-using-r/MLP backprop sigmoid MSE.R
@@ -1,71 +1,76 @@
 # Standard Backpropagation FeedForward Neural Network
-# 
-# Author: Gustavo (lgbpinho153@yahoo.com.br)
+
 ###############################################################################
 
-#
 # Samples 
 ##########
 
-I<-cbind(runif(100),runif(100),runif(100))
-T<-matrix(10+2*I[,1]+3*I[,2]+5*I[,3],100,1)
+# I<-cbind(runif(100),runif(100),runif(100))
+# T<-matrix(10 
+#           +2*I[,1]
+#           +3*I[,2]
+#           +5*I[,3],100,1)
 
 #Exclusive XOR
-#I<-cbind(c(0,0,1,1),c(0,1,0,1))
-#T<-cbind(c(0,1,1,0))
+I <- cbind(c(0,0,1,1), c(0,1,0,1))   # Input
+T <- cbind(c(0,1,1,0))               # Output
 
 #
 # Parameters
-##############
-N<-nrow(I) # number of samples
-NumInputs<-ncol(I)
-NumHidden<-2
-NumOutputs<-ncol(T)
-W1<-matrix(runif(NumHidden*NumInputs),NumInputs,NumHidden)
-b1<-matrix(runif(NumHidden),1,NumHidden)
-W1bar<-rbind(W1,b1)
-W2<-matrix(runif(NumOutputs*NumHidden),NumHidden,NumOutputs)
-b2<-matrix(runif(NumOutputs),1,NumOutputs)
-W2bar<-rbind(W2,b2)
-Lr<-0.05
-Mf<-0.05
+
+N <- nrow(I) # number of samples
+NumInputs <- ncol(I) # Number of variables. 
+NumHidden <- 2 # number of hidden layers
+NumOutputs <- ncol(T)
+
+# Weights from Input to Hidden
+W1 <- matrix(runif(NumHidden*NumInputs),NumInputs,NumHidden)
+b1 <- matrix(runif(NumHidden),1,NumHidden)
+W1bar<- rbind(W1,b1)
+
+# Weights from hidden to Output Layer
+W2 <- matrix(runif(NumOutputs*NumHidden),NumHidden,NumOutputs)
+b2 <- matrix(runif(NumOutputs),1,NumOutputs)
+W2bar <- rbind(W2,b2)
+
+
+Lr<-0.05  # Learning Rate
+Mf<-0.05  # ??
 NumEpochs<-1000
 
-#
+
 # Pre-process
-##############
-Inputs<-matrix(0,N,NumInputs)
-Targets<-matrix(0,N,NumOutputs)
+
+Inputs <- matrix(0,N,NumInputs)
+Targets <- matrix(0,N,NumOutputs)
+
 for (i in 1:NumInputs){
-	Inputs[,i]<-0.99*(I[,i]-min(I[,i]))/(max(I[,i])-min(I[,i]))+0.001	
+	Inputs[,i] <- 0.99 * (I[,i]-min(I[,i]))/(max(I[,i])-min(I[,i])) + 0.001	
 }
 for (i in 1:NumOutputs){
-	Targets[,i]<-0.99*(T[,i]-min(T[,i]))/(max(T[,i])-min(T[,i]))+0.001	
+	Targets[,i] <- 0.99 * (T[,i]-min(T[,i]))/(max(T[,i])-min(T[,i])) + 0.001	
 }
 
-#
 # Sigmoid
-###########
-phi<-function(v) 1/(1+exp(-v))
-phidash<-function(v) phi(v)*(1-phi(v))
+phi <- function(v) 1/(1+exp(-v))
+phidash <- function(v) phi(v)*(1-phi(v))
 
-#
 # Training
-############
-MSE<-rep(0,NumEpochs)
-Grad<-rep(0,NumEpochs)
-MinGrad<-1
-PreviousDW1bar<-0
-PreviousDW2bar<-0
+MSE <- rep(0,NumEpochs)
+Grad <- rep(0,NumEpochs)
+MinGrad <- 1
+PreviousDW1bar <- 0
+PreviousDW2bar <- 0
+
 for (epoch in 1:NumEpochs){
 	for(s in 1:N){ # adjusts are made after each sample
 		# forward pass
-		HiddenInputs<-c(Inputs[s,],1)
-		HiddenInducedFields<-HiddenInputs%*%W1bar
-		HiddenOutputs<-phi(HiddenInducedFields)
-		OutputInputs<-c(HiddenOutputs,1)
-		OutputInducedFields<-OutputInputs%*%W2bar
-		Outputs<-phi(OutputInducedFields)
+		HiddenInputs <- c(Inputs[s,],1)
+		HiddenInducedFields <- HiddenInputs%*%W1bar
+		HiddenOutputs <- phi(HiddenInducedFields)
+		OutputInputs <- c(HiddenOutputs,1)
+		OutputInducedFields <- OutputInputs%*%W2bar
+		Outputs <- phi(OutputInducedFields)
 		
 		#backpropagation
 		Delta2<-(Targets[s,]-Outputs)*Outputs*(1-Outputs) # local gradient for the output neurons
